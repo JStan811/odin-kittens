170024052c3ad9511a603da5120e46b83c7dec2a
Jake Standley <jakestandley95@gmail.com>
2021-10-26 16:23:34 -0500

    Complete section 1 'HTML'

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,53 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = "Success! Asgard thanks you."
+      redirect_to @kitten
+    else
+      flash.now[:error] = "By Odin's Raven! Fix your mistakes!"
+      render :new
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      flash[:success] = "Success! Asgard thanks you."
+      redirect_to @kitten
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
